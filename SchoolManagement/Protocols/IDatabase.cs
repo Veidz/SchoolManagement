89dd34124e3c707d719cfcddc138094ad5f26944
Veidz <joaovictorveidz@gmail.com>
2022-10-03 20:12:35 -0300

    feat: create IDatabase protocol

---
+++ SchoolManagement/Protocols/IDatabase.cs
@@ -0,0 +1,12 @@
+﻿using SchoolManagement.Models;
+using System.Collections.Generic;
+
+namespace SchoolManagement.Protocols
+{
+  public interface IDatabase
+  {
+    void Connect();
+    void Disconnect();
+    List<Student> GetAllStudents();
+  }
+}
