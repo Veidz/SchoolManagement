f306aaa17b456f386b0265d04f9388bb32dd38f4
Veidz <joaovictorveidz@gmail.com>
2022-10-03 20:02:33 -0300

    feat: create Student Model

---
+++ SchoolManagement/Models/Student.cs
@@ -0,0 +1,33 @@
+﻿namespace SchoolManagement.Models
+{
+  public class Student
+  {
+    private int id;
+    private string name;
+
+    public int ID
+    {
+      get { return id; }
+      set
+      {
+        id = value;
+      }
+    }
+
+    public string Name
+    {
+      get { return name; }
+      set
+      {
+        name = value;
+      }
+    }
+
+    public Student() { }
+    public Student(int id, string name)
+    {
+      ID = id;
+      Name = name;
+    }
+  }
+}
