1c27fd78c7d962599f0b10a9b9af653631868e2b
Veidz <joaovictorveidz@gmail.com>
2022-10-04 16:29:53 -0300

    feat: create Grade model

---
+++ SchoolManagement/Models/Grades.cs
@@ -0,0 +1,33 @@
+﻿namespace SchoolManagement.Models
+{
+  public class Grades
+  {
+    private int subjectID;
+    private int grade;
+
+    public int SubjectID
+    {
+      get { return subjectID; }
+      set
+      {
+        subjectID = value;
+      }
+    }
+
+    public int Grade
+    {
+      get { return grade; }
+      set
+      {
+        grade = value;
+      }
+    }
+
+    public Grades() { }
+    public Grades(int subjectID, int grade)
+    {
+      SubjectID = subjectID;
+      Grade = grade;
+    }
+  }
+}
