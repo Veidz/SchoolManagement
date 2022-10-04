527d87b3d6051f22705f11deb00a4203cecc0cb6
Veidz <joaovictorveidz@gmail.com>
2022-10-04 17:39:05 -0300

    feat: create MainViewModel

---
+++ SchoolManagement/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using SchoolManagement.Models;
+using System.Collections.ObjectModel;
+
+namespace SchoolManagement.ViewModels
+{
+  public class MainViewModel
+  {
+    public ObservableCollection<Student> Students { get; set; }
+
+    public MainViewModel()
+    {
+      Students = new ObservableCollection<Student>();
+
+      Student student = new Student()
+      {
+        ID = 1,
+        Name = "Joe Doe"
+      };
+
+      Students.Add(student);
+    }
+  }
+}
