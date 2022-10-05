62f2b8d14e60099c680e90222f373dba9aa07605
Veidz <joaovictorveidz@gmail.com>
2022-10-05 09:45:56 -0300

    test: CreateStudent method
    Ensure CreateStudent method works properly with invalid param length

--- SchoolManagement/Tests/Integration/StudentModel.cs
+++ SchoolManagement/Tests/Integration/StudentModel.cs
@@ -19,6 +19,18 @@ namespace SchoolManagement.Tests.Integration
     }
 
     [Test]
+    public void Ensure_CreateStudent_Works_Properly_With_Invalid_Param_Length()
+    {
+      DatabaseManager dbManager = new DatabaseManager(new MySQL());
+
+      Assert.That(() => dbManager.CreateStudent("aa"),
+        Throws.Exception
+          .TypeOf<Exception>()
+          .With.Property("Message")
+          .EqualTo("Name must be at least 3 characters long"));
+    }
+
+    [Test]
     public void Ensure_EditStudent_Works_Properly_With_Null_Param()
     {
       DatabaseManager dbManager = new DatabaseManager(new MySQL());
