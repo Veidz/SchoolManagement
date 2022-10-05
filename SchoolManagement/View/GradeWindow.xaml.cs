720158aac4c48fec0501325453b6966923529498
Veidz <joaovictorveidz@gmail.com>
2022-10-05 10:33:29 -0300

    feat: create GradeWindow

---
+++ SchoolManagement/View/GradeWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SchoolManagement.View
+{
+  public partial class GradeWindow : Window
+  {
+    public GradeWindow()
+    {
+      InitializeComponent();
+    }
+    public void AddGradeClick(object sender, RoutedEventArgs routedEventArgs)
+    {
+      DialogResult = true;
+    }
+  }
+}
